b740d3bb816fb8e21c1ecdaaacd00fc991ce1881
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-01 21:26:19 +0200

    adding setToken func

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,23 @@
 import axios from "axios";
 
 const phoneBookInstance = axios.create({
-    baseURL: 'https://connections-api.herokuapp.com/'
+    baseURL: 'https://connections-api.herokuapp.com/',
+
 })
+
+export const setToken = token =>{
+phoneBookInstance.defaults.headers.common.Authorization = `Bearer ${token}`
+}
 
 export const userRegister = async formData => {
     const {data} = await phoneBookInstance.post('/users/signup', formData)
+    setToken(data.token)
     return data
 }
 
 export const userLogin = async formData => {
     const {data} = await phoneBookInstance.post('/users/login', formData)
+    setToken(data.token)
     return data
 }
 
