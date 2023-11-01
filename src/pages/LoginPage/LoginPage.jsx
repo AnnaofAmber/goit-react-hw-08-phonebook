7707d20c169c6edc257bc2bd6ee46d60dae78027
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-01 21:21:16 +0200

    adding login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,39 @@
+import { useForm } from "react-hook-form"
+import { useDispatch } from "react-redux"
+import { loginThunk } from "redux/auth/operations"
+
+
 const LoginPage = () =>{
-    return (
-        <div></div>
-    )
-}
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+      } = useForm()
+
+const dispatch = useDispatch()
+
+      const onSubmit = (data) => {
+        dispatch(loginThunk(data)) 
+        reset()}
+    
+      return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                <span>Email:</span>
+            <input {...register("email", { required: true })} type="email"/>
+            {errors.email && <span>This field is required</span>}
+            </label>
+            <label>
+                <span>Password:</span>
+            <input {...register("password", { required: true , minLength:7})} type="password" />
+            {errors.password && <span>This field is required</span>}
+            </label>
+          
+            <button type="submit">Sing in</button>
+        </form>
+      )
+    }
+
 
 export default LoginPage
