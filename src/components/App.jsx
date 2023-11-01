6409735fec873405a39a01ca757cfbe8f8533fc8
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-01 19:53:02 +0200

    adding HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,18 @@
-import css from './App.module.css';
+// import css from './App.module.css';
 
-import { ContactForm } from './ContactForm/ContactForm';
-import { Contacts } from './Contacts/Contacts';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import { useDispatch, useSelector } from 'react-redux';
-import { getError, getIsLoading } from 'redux/contacts/selectors';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/operations';
-import { Loader } from './Loader/Loader';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Contacts } from './Contacts/Contacts';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getError, getIsLoading } from 'redux/contacts/selectors';
+// import { useEffect } from 'react';
+// import { fetchContacts } from 'redux/contacts/operations';
+// import { Loader } from './Loader/Loader';
 
 export const App = () => {
 
-  const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
   return (
-    <div className={css.main}>
-      <h1 className={css.title}>Phonebook</h1>
-      {isLoading && !error && <Loader/>}
-      {!isLoading && <div className={css.content}>
-        <ContactForm />
-      <Contacts title="Contacts">
-        <Filter />
-        <ContactList />
-      </Contacts>
-      </div>}
-    </div>
+    <div></div>
   );
 };
